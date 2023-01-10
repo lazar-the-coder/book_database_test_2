5bf514f194a161a436d0860c0ed2f5e69ed00120
lazar-the-coder <120413005+lazar-the-coder@users.noreply.github.com>
2023-01-10 14:24:16 -0600

    made the database

--- app.py
+++ app.py
@@ -1,1 +1,7 @@
 
+from models import Base, Book, engine, session
+
+
+
+if __name__ == '__main__':
+    Base.metadata.create_all(engine)
--- models.py
+++ models.py
@@ -1,1 +1,20 @@
+from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
+engine = create_engine('sqlite:///books.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+class Book(Base):
+    __tablename__ = 'books'
+    id = Column(Integer, primary_key = True)
+    title = Column('Title', String)
+    author = Column('Author', String)
+    published = Column('Date Published', Date)
+    medium = Column('Medium', String)
+    genre = Column('Genre', String)
+
+    def __repr__(self):
+        return f'Title: {self.title}\nAuthor: {self.author}\nDate: {self.published}\nMedium: {self.medium}\nGenre: {self.genre}'
